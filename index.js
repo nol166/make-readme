94b528bb461433d128c1edacd992e999fd74bcd6
John McCambridge <jmccamb@gmail.com>
2020-07-11 17:27:39 -0500

    fix markdown generator

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const { generateMd, writeToFile } = require('./utils/generateMarkdown')
+const { makeMd, writeToFile } = require('./utils/generateMarkdown')
 const questions = require('./src/questions.js')
 const { prompt } = require('inquirer')
 
 const ask = q => {
     prompt(q)
-        .then(a => writeToFile('README.md', generateMd(a)))
+        .then(a => writeToFile('README.md', makeMd(a)))
         .catch(e => {
             throw new Error(e)
         })
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,21 +1,82 @@
-class generateMd {
-    renderLicenseBadge = license =>
-        license !== 'none'
-            ? `![GitHub license](https://img.shields.io/badge/license-${license}-blue.svg)`
-            : ''
+const { writeFileSync } = require('fs')
+const path = require('path')
 
-    renderLicenseLink = license =>
-        license === 'none' ? `\n* [License](#license)\n` : ''
+const renderLicenseBadge = license =>
+    license !== 'None'
+        ? `![GitHub license](https://img.shields.io/badge/license-${license}-blue.svg)`
+        : ''
 
-    renderLicenseSection = license =>
-        license === 'none'
-            ? `## License
-  
-  This project is licensed under the ${license} license.`
-            : ''
+const renderLicenseLink = license =>
+    license === 'None' ? `\n* [License](#license)\n` : ''
+
+const renderLicenseSection = license => {
+    if (license === 'None') {
+        return `## License
+        
+        This project is licensed under the ${license} license.`
+    } else {
+        return ''
+    }
 }
 
 const writeToFile = (fileName, data) =>
-    fs.writeFileSync(path.join(process.cwd(), fileName), data)
+    writeFileSync(path.join(process.cwd(), fileName), data)
 
-module.exports = { generateMd, writeToFile }
+const makeMd = data => {
+    return `# ${data.project_name}
+  ${renderLicenseBadge(data.license)}
+  
+  ## Description
+  
+  ${data.description}
+  
+  ## Table of Contents 
+  
+  * [Installation](#installation)
+  
+  * [Usage](#usage)
+  ${renderLicenseLink(data.license)}
+  * [Contributing](#contributing)
+  
+  * [Tests](#tests)
+  
+  * [Questions](#questions)
+  
+  ## Installation
+  
+  To install necessary dependencies, run the following command:
+  
+  \`\`\`
+  ${data.installation}
+  \`\`\`
+  
+  ## Usage
+  
+  ${data.usage}
+  
+  ${renderLicenseSection(data.license)}
+    
+  ## Contributing
+  
+  ${data.contributing}
+  
+  ## Tests
+  
+  To run tests, run the following command:
+  
+  \`\`\`
+  ${data.test}
+  \`\`\`
+  
+  ## Questions
+  
+  If you have any questions about the repo, open an issue or contact me directly at ${
+      data.email
+  }. You can find more of my work at [${data.github}](https://github.com/${
+        data.github
+    }/).
+  
+  `
+}
+
+module.exports = { makeMd, writeToFile }
